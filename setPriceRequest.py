c2224496102aded2bc38581ff7b39b972c7b2fb8
Andre <67840136+agassi000@users.noreply.github.com>
2021-11-06 08:34:33 -0400

    Update setPriceRequest.py

--- setPriceRequest.py
+++ setPriceRequest.py
@@ -1,28 +1,60 @@
 from bs4 import BeautifulSoup
-import requests
+from tkinter import *
+import requests, webbrowser
 
-ticker = input("Please enter the ticker symbol --> ").strip()
-html_text = requests.get('https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol=' + ticker +'&ssoPageId=9&selectPage=1')
+ws = Tk()
+ws.title("Ticker Lookup")
+ws.geometry('500x500')
+ws['bg'] = '#ff10f0'
 
-soup = BeautifulSoup(html_text.content, 'html.parser')
+tiksym = Entry(ws)
+tiksym.pack(pady=30)
 
-StockLatestPriceRed = soup.find('div', class_='col-xs-6 col-xs-offset-6 colorRed')
-StockLatestPriceUnch = soup.find('div', class_='col-xs-6 col-xs-offset-6')
-StockLatestPriceGreen = soup.find('div', class_='col-xs-6 col-xs-offset-6 colorGreen')
 
-if StockLatestPriceRed is not None:
-    value1 = StockLatestPriceRed.text.strip()
-else:
-    value1 = ''
+def scrapedData():
+    ticker = tiksym.get()
+    html_text = requests.get('https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol=' + ticker +'&ssoPageId=9&selectPage=1')
 
-if StockLatestPriceGreen is not None:
-    value2 = StockLatestPriceGreen.text.strip()
-else:
-    value2 = ''
+    soup = BeautifulSoup(html_text.content, 'html.parser')
 
-if StockLatestPriceUnch is not None:
-    value3 = StockLatestPriceUnch.text.strip()
-else:
-    value3 = ''
+    StockLatestPriceRed = soup.find('div', class_='col-xs-6 col-xs-offset-6 colorRed')
+    StockLatestPriceUnch = soup.find('div', class_='col-xs-6 col-xs-offset-6')
+    StockLatestPriceGreen = soup.find('div', class_='col-xs-6 col-xs-offset-6 colorGreen')
 
-print('%s baht' % (value1 + value2 + value3).strip())
+    if StockLatestPriceRed is not None:
+      value1 = StockLatestPriceRed.text.strip()
+    else:
+      value1 = ''
+
+    if StockLatestPriceGreen is not None:
+      value2 = StockLatestPriceGreen.text.strip()
+    else:
+      value2 = ''
+
+    if StockLatestPriceUnch is not None:
+      value3 = StockLatestPriceUnch.text.strip()
+    else:
+      value3 = ''
+
+    if value1 + value2 + value3 is '':
+      Label(ws, text="There is no "+ ticker.upper() + " in the SET (The Stock Exchange of Thailand)", pady=5, bg='#ff10f0').pack()
+    else:
+      web = 'https://www.settrade.com/C04_01_stock_quote_p1.jsp?txtSymbol=' + ticker + '&ssoPageId=9&selectPage=1'
+      Label(ws, text=ticker.upper() + " is "+'%s baht' % (value1 + value2 + value3), pady=5, bg='#ff10f0').pack()
+      link = Label(ws, text="Check it by clicking here", fg="blue", cursor="hand2")
+      link.pack()
+      link.bind("<Button-1>", lambda e:
+      callback(web))
+
+    def callback(url):
+      webbrowser.open_new_tab(url)
+
+Button(
+    ws,
+    text="Lookup!!!",
+    padx=10,
+    pady=5,
+    command=scrapedData
+    ).pack()
+
+ws.mainloop()
